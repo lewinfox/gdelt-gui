32ef1fcd41b6c02e48d037de15bc83a5c5ee1fcd
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2019-04-11 21:32:17 +0100

    Working prototype

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,39 @@
-import './../css/master.css';  // Required to ensure that CSS is incorporated
+console.log('index.js connected');
+
+// Page components
+const components = {}
+
+
+const buildString = (endpoint, query, mode) => {
+
+    // Building the string
+    let queryString = `${endpoint}query="${query}"&mode=${mode}`;
+    console.log(queryString);
+    return queryString;
+}
+
+const pointIframeAt = (url) => {
+    components.iFrame.src = url;
+}
+
+const main = (e) => {
+    e.preventDefault();
+    const endpoint = components.endpoint;
+    const query = components.queryInput.value;
+    const mode = components.mode.value;
+    const url = buildString(endpoint, query, mode)
+    pointIframeAt(url);
+}
+
+window.onload = () => {
+    components.queryInput = document.getElementById('query-input');
+    components.mode = document.getElementById('mode-input');
+    components.submitButton = document.getElementById('submit-button');
+    components.iFrame = document.getElementById('iframe');
+
+    // URL building components
+    components.endpoint = 'https://api.gdeltproject.org/api/v2/doc/doc?';
+
+    // Event listener
+    components.submitButton.addEventListener('click', main);
+}
